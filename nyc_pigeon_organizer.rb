67e11de0136a15fbe86034521bda597ac120f2e3
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-22 22:33:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,9 @@ require'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key , value), final_array|
-    binding.pry
+    #binding.pry
     value.each do |inner_key, names|
-      b
+      binding.pry
     final_array
   end
   binding.pry
