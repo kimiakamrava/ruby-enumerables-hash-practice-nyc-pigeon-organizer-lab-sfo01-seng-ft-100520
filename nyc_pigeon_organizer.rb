6a5f465367ee19c5608bca7238609305bef86d1d
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-22 22:37:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,9 +5,10 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key , value), final_array|
     #binding.pry
     value.each do |inner_key, names|
-      
-      names.each do |name|
+     names.each do |name|
+        binding.pry
     end
+  end
     final_array
   end
   binding.pry
