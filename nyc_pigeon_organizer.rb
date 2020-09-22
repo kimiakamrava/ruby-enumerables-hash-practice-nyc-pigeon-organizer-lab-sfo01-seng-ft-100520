e47d4521c11ca21e9bb5eb0fb239c5ed97ded0ff
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-22 22:29:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ require'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.eacg_with_object({}) do |(key , value), final_array|
+    binding.pry
     final_array
   binding.pry
 end
